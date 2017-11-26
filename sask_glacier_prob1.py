cc7add4c73a01a650d3b533cce300b4f9b10e528
Dave Whipp <davewhipp@users.noreply.github.com>
2017-11-27 00:13:07 +0200

    Update sask_glacier_prob1.py

--- sask_glacier_prob1.py
+++ sask_glacier_prob1.py
@@ -17,47 +17,6 @@ Author:
 import numpy as np
 import matplotlib.pyplot as plt
 
-#--- User-defined variables ---
-a =                                     # Viscosity [1/(Pa**3.0 s)]
-h =                                     # Channel width
-y = np.linspace(-h/2.0, h/2.0, 101)     # Range of values across channel for velocity calculation
-n_prof = 5                              # Number of velocity profiles to calculate
-
-# Equations
-u_nd = np.zeros([n_prof, len(y)])
-n = 0
-
-# Non-dimensional velocity profile for a Newtonian or non-Newtonian fluid
-# Loop over all values of the power-law exponent
-for i in range():
-    n = 
-    # Loop over all values of the width of the channel
-    for j in range():
-        if :
-                                 # Equation 12, modified
-        else:
-                                 # Equation 12
-
-# Calculate maximum percent change in velocity between n=2 and n=4
-# Uncomment this line to add in the calculation
-#max_percent_change = 
-
-# Plot results
-# Loop over all values of the power-law exponent
-for i in range(n_prof):
-    plt.plot()
-    plt.text()
-    
-# Add axis labels and a title
-plt.xlabel("")
-plt.ylabel("")
-plt.title("")
-plt.show()
-
-# Import NumPy
-import numpy as np
-import matplotlib.pyplot as plt
-
 #--- User-defined variables
 a =                                     # Viscosity [1/(Pa**3.0 s)]
 h =                                     # Channel width
@@ -69,9 +28,9 @@ n_prof = 4                              # Number of velocity profiles to calcula
 data = np.loadtxt(fname='sask_glacier_velo.txt', delimiter=',')
 
 # Split file data columns into different variables
-data_y = 
-data_u_ma = 
-data_u_ms = 
+data_y =            # Array for data positions across channel width [m]
+data_u_ma =         # Array for data velocities [m/a]
+data_u_ms =         # Array for data velocities [m/s]
 
 # Equations
 u = np.zeros([n_prof, len(y)])
