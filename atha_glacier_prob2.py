6697dfef6293d1b2c7a818de089f48e7ad15e91a
Dave Whipp <davewhipp@users.noreply.github.com>
2017-11-27 00:10:56 +0200

    Update atha_glacier_prob2.py

--- atha_glacier_prob2.py
+++ atha_glacier_prob2.py
@@ -1,9 +1,16 @@
-"""
-atha_glacier_prob2.py
+#!/usr/bin/env python3
+"""Calculates flow velocities through a valley glacier cross-section.
 
-This script ...
-
-@author: NAME - DD.MM.YYYY
+Description:
+    This script calculates and plots the solution for dimensional velocity as
+    a function of dimensional distance from the base of a valley glacier. It also
+    loads and plots observed flow velocities from a file.
+    
+Usage:
+    ./atha_glacier_prob2.py
+    
+Author:
+    XXX YYY - DD.MM.YYYY
 """
 
 # Import NumPy
@@ -13,45 +20,41 @@ import matplotlib.pyplot as plt
 # Open and read input file
 data = np.loadtxt(fname='atha_glacier_velo.txt', delimiter=',')
 
-# Create zeros arrays
-data_depth = np.zeros(len(data))  # Create empty array for data depths [m]
-data_z = np.zeros(len(data))      # Create empty array for data elevations from bed [m]
-data_u_ma = np.zeros(len(data))   # Create empty array for data velocities [m/a]
-data_u_ms = np.zeros(len(data))   # Create empty array for data velocities [m/s]
-
-# Loop over lines in file and split into different variables
-for line in data:
-    data_depth[linecount] = line[0]
-    data_z[linecount] = line[1]
-    data_u_ma[linecount] = line[2]
-    data_u_ms[linecount] = line[3]
+# Split file data columns into different variables using data slices
+data_depth =        # Array for data depths [m]
+data_z =            # Array for data elevations from bed [m]
+data_u_ma =         # Array for data velocities [m/a]
+data_u_ms =         # Array for data velocities [m/s]
 
 #--- User-defined variables
-a =                    # Viscosity [1/(Pa**3.0 s)]
-h =                    # Flow height
-z =                    # Range of elevation values in flow for velocity calculation
-data_umax =            # Velocity at top of flow
-data_ub =              # Velocity at base of flow
-n_prof =               # Number of velocity profiles to calculate
+a =                             # Viscosity [1/(Pa**3.0 s)]
+h =                             # Flow height
+z = np.linspace(0, h, 101)      # Range of elevation values in flow for velocity calculation
+data_umax =                     # Velocity at top of flow
+data_ub =                       # Velocity at base of flow
+n_prof = 5                      # Number of velocity profiles to calculate
 
 # Equations
-u = np.zeros([n_prof,len()])
+u = np.zeros([n_prof, len(z)])
 n = 0
 
-# Velocity profile for a Newtonian or non-Newtonian fluid
+# Non-dimensional velocity profile for a Newtonian or non-Newtonian fluid
+# Loop over all values of the power-law exponent
 for i in range():
-    n =
+    n = 
     if n == 1:
-
+        
     else:
-        ub =
-
-    for j in range(len(z)):
+        ub = 
+    # Loop over all values of the height of the channel
+    for j in range():
                                 # Equation 19 rearranged to solve for gamma_x
                                 # Equation 19
 
-# Plot predicted velocity profiles
-plt.plot()
+# Plot results
+# Loop over all values of the power-law exponent
+for i in range(n_prof):
+    plt.plot()
 
 # Plot observed velocities
 plt.plot()
